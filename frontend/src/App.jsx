2fb25b30be35097d7b4276fe7e6ad3de132179cd
Johannes Kardenbach <johannes.kardenbach@edu.rptu.de>
2026-04-22 12:35:19 +0200

    new app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,121 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+// Define all issue components
+type Issue = {
+  title: string;
+  description: string;
+  location: string;
 }
 
-export default App
+export default function App() {
+
+  // Input
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [location, setLocation] = useState('');
+
+  // List of issues
+  const [issueList, setIssueList] = useState<Issue[]>([]);
+
+  // Add issue to list
+  const addIssue = (event: React.SubmitEvent) => {
+
+    // Stops refreshing
+    event.preventDefault(); 
+
+    // Dont add empty issue to Array
+    if (title === '') return; 
+
+    // Combine into new Issue
+    const newIssue: Issue = {
+      title: title,
+      description: description,
+      location: location
+    };
+
+    // Add Issue to Array
+    setIssueList([...issueList, newIssue]); 
+
+    // Clear Input
+    setTitle(''); 
+    setDescription(''); 
+    setLocation('');
+  }
+
+  // UI
+  return (
+    <div>
+      <h1>Mängelmelder</h1>
+
+      {/* Input form */}
+      <form onSubmit={addIssue} style={{ display: 'flex', flexDirection: 'column', gap: '10px', maxWidth: '300px', margin: '0 auto' }}>
+
+        <input type="text" placeholder="Title" value={title} onChange={(event) => setTitle(event.target.value)}/>
+        <input type="text" placeholder="Beschreibung" value={description} onChange={(event) => setDescription(event.target.value)} rows={4}/>
+        <input type="text" placeholder="Ort" value={location} onChange={(event) => setLocation(event.target.value)}/>
+
+        <button type="submit">Hinzufügen</button>
+      </form>
+
+      {/* List of issues */}
+      <ul style={{ listStyleType: 'none', padding: 0 }}>
+        {issueList.map((issue, index) => (
+          <li key={index}>
+            <h3>{issue.title}</h3>
+            <p>{issue.description}</p>
+            <p>{issue.location}</p>
+          </li>
+        ))}
+      </ul>
+
+    </div>
+
+
+  );
+
+}

5fb66f4d15a61b6e8f835983214fc152330b4e05
Daniel Cairoli <d_cairoli23@scilab-0069.informatik.uni-kl.de>
2026-04-29 21:45:21 +0200

    sci server als allowed host

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,6 +9,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src/frontend'),
       '@shared': path.resolve(__dirname, './src/shared'),
     },
+  },
+  server: {
+    allowedHosts: ['scilab-0069.informatik.uni-kl.de'],
   },
   build: {
     outDir: 'dist/frontend',

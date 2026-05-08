d463dc29a4be3113548ff410d0d256c14c4eb94a
Daniel Cairoli <daniel.cairoli@icloud.com>
2026-05-08 12:45:01 +0200

    titel auskommentiert

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -215,7 +215,7 @@ export default function App() {
     minMatchCharLength: 3,
     keys: [
       { name: "location", weight: 0.4 },
-      //{ name: "title", weight: 0.25 },
+      //{ name: "title", weight: 0.25 }, titel auskommentiert
       { name: "description", weight: 0.25 },
       { name: "user_email", weight: 0.1 },
     ],
@@ -230,7 +230,7 @@ export default function App() {
   function issueMatchesShortSearch(issue: Issue) {
     const searchableValues = [
       issue.location,
-      issue.title,
+      //issue.title, // titel auskommentiert
       issue.description,
       issue.user_email,
       issue.kategorie,

3c79feb6fb26f354fbe80d0f201e7ac7c3964d3a
p_hall24 <vux77muq@rptu.de>
2026-05-23 09:44:41 +0200

    settings button html ausgelagert

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -44,7 +44,7 @@ import { RegistrationLogin } from './library/ui/ERRORregistrationLogin';
 import { InputForm } from './library/ui/inputForm';
 import { ViewModeButtons } from './library/ui/viewModeButtons';
 import { registerClient } from 'fuse/next/server';
-
+import { SettingsButton } from './library/ui/settingsButton';
 
 
 
@@ -60,7 +60,7 @@ export default function App() {
   
   const{loadIssues, deleteIssue}=useLoadIssues(setIssueList);
   
-  // Input      wird nicht mehr benötigt, ist das schlimm, mit dem fehlenden loadIssues und setIsArchiveMode
+  // Input      wird nicht mehr benötigt, ist das schlimm, mit dem fehlenden loadIssues und setIsArchiveMode ?
   // const{title, setTitle,description, setDescription, location, setLocation, kategorie, setKategorie, image, setImage, addIssue}=useInput(() => {
   //   loadIssues(false);
   //   setIsArchiveMode(false);
@@ -289,16 +289,17 @@ export default function App() {
       setSettingsMessage(data.message || "Verifizierungs-E-Mail wurde gesendet");
     };
     
-    const settingsButton = (
-      <button className="settings-button" type="button" aria-label="Einstellungen öffnen" title="Einstellungen" onClick={() => setSettingsOpen(true)}>
-      <svg className="settings-icon" viewBox="0 0 24 24" aria-hidden="true">
-      <path d="M19.4 13.5c.1-.5.1-1 .1-1.5s0-1-.1-1.5l2-1.5-2-3.5-2.4 1a8 8 0 0 0-2.6-1.5L14 2h-4l-.4 3a8 8 0 0 0-2.6 1.5l-2.4-1-2 3.5 2 1.5A9.4 9.4 0 0 0 4.5 12c0 .5 0 1 .1 1.5l-2 1.5 2 3.5 2.4-1a8 8 0 0 0 2.6 1.5l.4 3h4l.4-3a8 8 0 0 0 2.6-1.5l2.4 1 2-3.5-2-1.5ZM12 15.5a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7Z" />
-      </svg>
-      </button>
-    );
+    //  => ausgelagert in settingsButton.tsx
+    // const settingsButton = (
+    //   <button className="settings-button" type="button" aria-label="Einstellungen öffnen" title="Einstellungen" onClick={() => setSettingsOpen(true)}>
+    //   <svg className="settings-icon" viewBox="0 0 24 24" aria-hidden="true">
+    //   <path d="M19.4 13.5c.1-.5.1-1 .1-1.5s0-1-.1-1.5l2-1.5-2-3.5-2.4 1a8 8 0 0 0-2.6-1.5L14 2h-4l-.4 3a8 8 0 0 0-2.6 1.5l-2.4-1-2 3.5 2 1.5A9.4 9.4 0 0 0 4.5 12c0 .5 0 1 .1 1.5l-2 1.5 2 3.5 2.4-1a8 8 0 0 0 2.6 1.5l.4 3h4l.4-3a8 8 0 0 0 2.6-1.5l2.4 1 2-3.5-2-1.5ZM12 15.5a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7Z" />
+    //   </svg>
+    //   </button>
+    // );
     
     
-    // // Delete issue
+    // // Delete issue => in useLoadIssue
     // const deleteIssue = async (id: number) => {
     //   await fetch(`/api/mangel/${id}`, { method: 'DELETE' });
     //   loadIssues();
@@ -371,14 +372,18 @@ export default function App() {
         {/* Wenn man eingeloggt ist: logout und einstellungen*/}
         <span className="auth-status">Eingeloggt als <strong>{userEmail}</strong> ({userRole === "admin" ? "Admin" : "Nutzer"})</span>
         <button className='logout-button' onClick={logout}>Logout</button>
-        {settingsButton}
+        <SettingsButton     //ausgelagert, braucht setSettingsOpen
+        setSettingsOpen={setSettingsOpen}
+        />
         </div>
       ) : (
         <div className="auth-bar">
         {/* Wenn man nicht eingeloggt ist: login und register */}
         <button onClick={() => setAuthView(authView === "login" ? null : "login")}>Login</button>
         <button onClick={() => setAuthView(authView === "register" ? null : "register")}>Registrieren</button>
-        {settingsButton}
+        <SettingsButton     //ausgelagert, braucht setSettingsOpen
+        setSettingsOpen={setSettingsOpen}
+        />
         </div>
       )}
       
---
+++ src/frontend/library/ui/settingsButton.tsx
@@ -0,0 +1,9 @@
+export function SettingsButton({setSettingsOpen}:{setSettingsOpen: React.Dispatch<React.SetStateAction<boolean>>}){
+    return(
+      <button className="settings-button" type="button" aria-label="Einstellungen öffnen" title="Einstellungen" onClick={() => setSettingsOpen(true)}>
+      <svg className="settings-icon" viewBox="0 0 24 24" aria-hidden="true">
+      <path d="M19.4 13.5c.1-.5.1-1 .1-1.5s0-1-.1-1.5l2-1.5-2-3.5-2.4 1a8 8 0 0 0-2.6-1.5L14 2h-4l-.4 3a8 8 0 0 0-2.6 1.5l-2.4-1-2 3.5 2 1.5A9.4 9.4 0 0 0 4.5 12c0 .5 0 1 .1 1.5l-2 1.5 2 3.5 2.4-1a8 8 0 0 0 2.6 1.5l.4 3h4l.4-3a8 8 0 0 0 2.6-1.5l2.4 1 2-3.5-2-1.5ZM12 15.5a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7Z" />
+      </svg>
+      </button>
+      )
+}

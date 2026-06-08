e03281185a49b2a9bb1b93e518355898bd89da61
Daniel Cairoli <daniel.cairoli@icloud.com>
2026-06-08 15:58:18 +0200

    bugfix: mangel posten lädt karte wieder neu

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1074,8 +1074,9 @@ export default function App() {
             <h2 style={{ marginBottom: '20px' }}>Mangel melden</h2>
             {/* Input form nur sichtbar wenn man eingeloggt ist*/}
             <InputForm
-              onIssueCreated={() => {
+              onIssueCreated={async () => {
                 loadIssuePage(1, false);
+                await refreshMapData();
                 setShowInputModal(false);
                 toast.success("Mangel erfolgreich gemeldet");
               }}
